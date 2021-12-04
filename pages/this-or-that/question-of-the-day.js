fbc815b76806d55c8003c15aa0d2422df67672c6
Niraj Gautam <nirajgautam@Nirajs-MacBook-Pro.local>
2021-12-04 00:40:04 -0500

    CSS

--- pages/this-or-that/question-of-the-day.js
+++ pages/this-or-that/question-of-the-day.js
@@ -1,20 +1,29 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
-export default function FirstPost() {
-    return <h1 className="title">
-            <Link href="/this-or-that/question-of-the-day"><a>TEA</a></Link> or{' '}
-            <Link href="/this-or-that/question-of-the-day"><a>COFFEE</a></Link>
-            <style jsx>{`
-            .title a {
-              color: #0070f3;
-              text-decoration: none;
-            }
-            .title a:hover,
-            .title a:focus,
-            .title a:active {
-              text-decoration: underline;
-            }`}</style>
-            </h1>
+export default function QuestionOfTheDay() {
+    return <Layout>
+              <div className="container">
+                <h1 className="title">
+                  <Head>
+                    <title>THIS or THAT</title>
+                  </Head>
+                  <Link href="/this-or-that/question-of-the-day"><a>TEA</a></Link> or{' '}
+                  <Link href="/this-or-that/question-of-the-day"><a>COFFEE</a></Link>
+                  <style jsx>{`
+                  .title a {
+                    color: #0070f3;
+                    text-decoration: none;
+                  }
+                  .title a:hover,
+                  .title a:focus,
+                  .title a:active {
+                    text-decoration: underline;
+                  }`}</style>
+                </h1>
+              </div>
+            </Layout>
             
   }
   
